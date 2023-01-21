9e272fd135743d16f415d2cf1cef6458757ada17
Sergio Hernández <sehb1234@gmail.com>
2023-01-20 21:42:37 -0500

    Anexo botón

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <div className="App">
-      En construcción
+      <h1> En construcción </h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
